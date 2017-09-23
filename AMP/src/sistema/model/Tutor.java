8723d5580fae2bcfb7e014c2dd32fbedc6f629c2
vicious721 <du-du7@hotmail.com>
2017-09-22 21:07:16 -0300

    Update Tutor.java

--- AMP/src/sistema/model/Tutor.java
+++ AMP/src/sistema/model/Tutor.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistema.model;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- *
- * @author Roberto Bolgheroni
- */
 public class Tutor extends Logavel {
 
     private final int idTutor;
